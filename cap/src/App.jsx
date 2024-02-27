298f5a69e02355ec41cd0742454a0184af1b423d
FrederickIngle <fingle2016@fau.edu>
2024-02-27 01:45:32 -0500

    checkpoint 2, screenshot functionality is not working

--- cap/src/App.jsx
+++ cap/src/App.jsx
@@ -3,7 +3,10 @@ import './App.css'
 import APIForm from './components/APIForm.jsx'
 const ACCESS_KEY = import.meta.env.VITE_APP_ACCESS_KEY;
 
-// State Variables
+
+function App() {
+
+  // State Variables
 const [inputs, setInputs] = useState({
   url: "",
   format: "",
@@ -12,14 +15,69 @@ const [inputs, setInputs] = useState({
   width: "",
   height: "",
 }); 
+const reset = () => {
+  setInputs({
+    url: "",
+    format: "",
+    no_ads: "",
+    no_cookie_banners: "",
+    width: "",
+    height: "",
+  });
+}
 
-function App() {
+const [currentImage, setCurrentImage] = useState(null);
  
-  const submitForm = () => {
+  const submitForm = (e) => {
     e.preventDefault();
- 
+    let defaultValues = {
+      format: "jpeg",
+      no_ads: "true",
+      no_cookie_banners: "true",
+      width: "1920",
+      height: "1080",
+    };
 
+    if (inputs.url == "" || inputs.url == " ") {
+      alert("You forgot to submit a url!");
+    } else {
+      for (const [key, value] of Object.entries(inputs)) {
+        if (value == "") {
+          inputs[key] = defaultValues[key]
+        }
+      }
+      makeQuery();
+    }
   }
+
+  const makeQuery = () => {
+    let wait_until = "network_idle";
+    let response_type = "json";
+    let fail_on_status = "400%2C404%2C500-511";
+    let url_starter = "https://";
+    let fullURL = url_starter + inputs.url;
+  
+    let query = `https://api.apiflash.com/v1/urltoimage?access_key=${ACCESS_KEY}&url=${fullURL}&format=${inputs.format}&width=${inputs.width}&height=${inputs.height}&no_cookie_banners=${inputs.no_cookie_banners}&no_ads=${inputs.no_ads}&wait_until=${wait_until}&response_type=${response_type}&fail_on_status=${fail_on_status}`;
+  
+    callAPI(query).catch(console.error);
+  }
+  
+
+  const callAPI = async (query) => {
+    try {
+      const response = await fetch(query);
+      if (!response.ok) {
+        console.error('API response error:', response.status, response.statusText);
+        // You can also log response.text() or response.body here for more details
+      }
+      const json = await response.json();
+      // ... rest of your logic
+    } catch (error) {
+      console.error('API call error:', error);
+    }
+  }
+  
+  
 
   return (
     <div className="whole-page">
@@ -35,6 +93,34 @@ function App() {
         }
         onSubmit={submitForm}
       />
+      {currentImage ? (
+          <img
+            className="screenshot"
+            src={currentImage}
+            alt="Screenshot returned"
+          />
+        ) : (
+          <div> </div>
+        )}
+
+      <div className="container">
+        <h3> Current Query Status: </h3>
+        <p>
+          https://api.apiflash.com/v1/urltoimage?access_key=ACCESS_KEY    
+          <br></br>
+          &url={inputs.url} <br></br>
+          &format={inputs.format} <br></br>
+          &width={inputs.width}
+          <br></br>
+          &height={inputs.height}
+          <br></br>
+          &no_cookie_banners={inputs.no_cookie_banners}
+          <br></br>
+          &no_ads={inputs.no_ads}
+          <br></br>
+        </p>
+      </div>
+
       <br></br>
 
     </div>
